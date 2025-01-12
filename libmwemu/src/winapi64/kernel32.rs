0744821d55a857a8b2544655fd01da90219abc68
Brandon Ros <brandonros1@gmail.com>
2025-01-12 14:03:48 -0500

    CreateFile

--- libmwemu/src/winapi64/kernel32.rs
+++ libmwemu/src/winapi64/kernel32.rs
@@ -3713,8 +3713,11 @@ fn CreateFileA(emu: &mut emu::Emu) {
     let dw_flags_and_attributes = emu.regs.r11 as usize;
     let h_template_file = emu.regs.r12 as usize;
     log_red!(emu, "** {} kernel32!CreateFileA lp_file_name: 0x{:x} dw_desired_access: 0x{:x} dw_share_mode: 0x{:x} lp_security_attributes: 0x{:x} dw_creation_disposition: 0x{:x} dw_flags_and_attributes: 0x{:x} h_template_file: 0x{:x}", emu.pos, lp_file_name, dw_desired_access, dw_share_mode, lp_security_attributes, dw_creation_disposition, dw_flags_and_attributes, h_template_file);
-    // TODO: Implement this
-    emu.regs.rax = 1;
+    let mut name: String = String::new();
+    if lp_file_name > 0 {
+        name = emu.maps.read_string(lp_file_name as u64);
+    }
+    emu.regs.rax = helper::handler_create(&name);
 }
 
 /*
