2f0780d3a0cd3126a430efb0717d9560ac95a428
Brandon Ros <brandonros1@gmail.com>
2025-08-16 20:56:42 -0400

    CURRENT_THREAD_PSEUDO_HANDLE

--- crates/libmwemu/src/winapi/winapi64/kernel32/get_current_thread.rs
+++ crates/libmwemu/src/winapi/winapi64/kernel32/get_current_thread.rs
@@ -3,5 +3,10 @@ use crate::emu;
 
 pub fn GetCurrentThread(emu: &mut emu::Emu) {
     log_red!(emu, "** {} kernel32!GetCurrentThread", emu.pos);
-    panic!("TODO");
+    
+    // GetCurrentThread returns a pseudo-handle (-2 or 0xFFFFFFFFFFFFFFFE in 64-bit)
+    // This is a special constant that always refers to the current thread
+    const CURRENT_THREAD_PSEUDO_HANDLE: u64 = 0xFFFFFFFFFFFFFFFE; // -2 as u64
+    
+    emu.regs_mut().rax = CURRENT_THREAD_PSEUDO_HANDLE;
 }
